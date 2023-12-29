f5415cb7b06b63438ee67a96f2b7eb4300cf5d73
expog2000 <hariom@pearlthoughts.com>
2023-12-29 14:41:36 +0530

    npm installed

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,7 +5,7 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('hello-wrold')
+  @Get('hello-world')
   getHello(): string {
     return this.appService.getHello();
   }
